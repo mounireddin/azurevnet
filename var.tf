f395b06dcabaa9a199eabc55d397f9b378de46ae
Mounir <mounir@mobilabsolutions.com>
2023-05-24 16:30:13 +0200

    initial commit

--- var.tf
+++ var.tf
@@ -1,13 +1,3 @@
 variable "name" {
   description = "(Required) The name of the Resource Group."
 }
-
-variable "location" {
-  description = "(Required) The location of the Resource Group."
-}
-
-variable "tags" {
-  description = "(Optional) The tags for the Resource Group."
-
-  default = {}
-}
