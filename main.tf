8de84a61b26f7f275ea62a7c894e51cd99966d9c
Mounir <mounir@mobilabsolutions.com>
2023-06-29 09:00:04 +0200

    cc

--- main.tf
+++ main.tf
@@ -1,5 +1,18 @@
-resource "azurerm_resource_group" "resource_group" {
-  name     = var.name
-  location = "west europe"
+resource "azurerm_network_security_group" "network_security_group" {
+  name                = var.name
+  location            = "west europe"
+  resource_group_name = "pankaj"
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
   tags     = {}
 }
