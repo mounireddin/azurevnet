7159937c1ed8f3e3b2564288de0784a378bdd8ca
Mounir <mounir@mobilabsolutions.com>
2023-06-30 14:31:06 +0200

    whatever

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_network_security_group" "network_security_group" {
   name                = var.name
   location            = "west europe"
-  resource_group_name = "pankaj"
+  resource_group_name = "mounir_rg"
 
   security_rule {
     name                       = "test123"
